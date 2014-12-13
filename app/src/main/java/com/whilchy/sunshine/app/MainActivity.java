80a519dcd0414be827062b1489fd971ce5194ed2
theFrantic <whilchyjd@gmail.com>
2014-12-13 15:57:52 +0000

    Set the ArrayAdapter to the ListView

--- app/src/main/java/com/whilchy/sunshine/app/MainActivity.java
+++ app/src/main/java/com/whilchy/sunshine/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -82,6 +83,9 @@ public class MainActivity extends ActionBarActivity {
             );
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            forecastListView.setAdapter(forecastAdapter);
 
             return rootView;
         }
